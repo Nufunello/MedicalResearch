fbc5b92b8187a6eabeef0f972b626e6ee971a8b6
Yuliia-077 <62853554+Yuliia-077@users.noreply.github.com>
2022-04-11 20:52:14 +0300

    Add new projects and add dependies between them

---
+++ Application/FactoryOptimizer/MedicalResearch.Persistence/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MedicalResearch.Persistence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
