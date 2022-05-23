8d7d727504f68e961d87da65e52667e0768f72c0
Yuliia-077 <62853554+Yuliia-077@users.noreply.github.com>
2022-05-24 00:34:22 +0300

    create research

---
+++ Application/FactoryOptimizer/MedicalResearch/Controllers/ResearchController.cs
@@ -0,0 +1,36 @@
+﻿using MedicalResearch.Application.Services.Regions;
+using MedicalResearch.Application.Services.Researches;
+using MedicalResearch.Models.Regions;
+using MedicalResearch.Models.Researches;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalResearch.Controllers
+{
+    [ApiController]
+    [Route("researches")]
+    public class ResearchController : ControllerBase
+    {
+        private readonly IResearchService _researchService;
+        public ResearchController(IResearchService researchService)
+        {
+            _researchService = researchService;
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> GetResearches([FromBody]ResearchListQuery query)
+        {
+            var queryDto = query.AsDto();
+            var itemsDto = await _researchService.GetResearchListItems(queryDto);
+            var itemsVM = itemsDto.Select(i => i.AsViewModel());
+            return Ok(itemsVM);
+        }
+
+        [HttpPost("new")]
+        public async Task<IActionResult> Create([FromBody] ResearchCreateRequestModel model)
+        {
+            return Ok(await _researchService.Create(model.AsDto()));
+        }
+    }
+}
---
+++ Application/FactoryOptimizer/MedicalResearch/Models/Researches/ResearchListQuery.cs
@@ -0,0 +1,24 @@
+﻿using MedicalResearch.Application.Models.Researches.Dto;
+
+namespace MedicalResearch.Models.Researches
+{
+    public class ResearchListQuery
+    {
+        public int? GroupID { get; set; }
+        public string GroupName { get; set; }
+        public string Name { get; set; }
+    }
+
+    public static partial class ViewModelMapperExtensions
+    {
+        public static ResearchListQueryDto AsDto(this ResearchListQuery requestModel)
+        {
+            return new ResearchListQueryDto
+            {
+                GroupID = requestModel.GroupID,
+                GroupName = requestModel.GroupName,
+                Name = requestModel.Name
+            };
+        }
+    }
+}
