7383e5e5d241262118e63294e319acb490f1bc1f
Yuliia-077 <62853554+Yuliia-077@users.noreply.github.com>
2022-05-24 01:53:11 +0300

    update and get research

---
+++ Application/FactoryOptimizer/MedicalResearch.Application/Models/Researches/Dto/ResearchCreateUpdateDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MedicalResearch.Application.Models.Researches.Dto
+{
+    public class ResearchCreateUpdateDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DeadlineInDays { get; set; }
+        public decimal Cost { get; set; }
+        public string PreparationDescription { get; set; }
+        public int GroupResearchID { get; set; }
+    }
+}
---
+++ Application/FactoryOptimizer/MedicalResearch/Models/Researches/Research.cs
@@ -0,0 +1,38 @@
+﻿using MedicalResearch.Application.Models.Researches.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicalResearch.Models.Researches
+{
+    public class Research
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DeadlineInDays { get; set; }
+        public decimal Cost { get; set; }
+        public string PreparationDescription { get; set; }
+        public int GroupID { get; set; }
+        public string GroupName { get; set; }
+    }
+
+    public static partial class ViewModelMapperExtensions
+    {
+        public static Research AsViewModel(this ResearchDto dto)
+        {
+            return new Research
+            {
+                ID = dto.ID,
+                Name = dto.Name,
+                Description = dto.Description,
+                DeadlineInDays = dto.DeadlineInDays,
+                Cost = dto.Cost,
+                PreparationDescription = dto.PreparationDescription,
+                GroupID = dto.GroupID,
+                GroupName = dto.GroupName
+            };
+        }
+    }
+}
